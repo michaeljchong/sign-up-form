b35a442ce70d9e76c0a7b5362d34ecd872015439
Michael Chong <mikec0920@gmail.com>
2022-02-16 15:05:54 -0800

    Toggle error class with password validation

--- script.js
+++ script.js
@@ -4,10 +4,19 @@ let confirmPassword = document.querySelector("#confirm_password");
 function checkPassword() {
   if (password.value != confirmPassword.value) {
     confirmPassword.setCustomValidity("Passwords do not match");
+    if (!password.classList.contains("error")) {
+      password.classList.toggle("error");
+      confirmPassword.classList.toggle("error");
+    }
   } else {
     confirmPassword.setCustomValidity("");
+    if (password.classList.contains("error")) {
+      password.classList.toggle("error");
+      confirmPassword.classList.toggle("error");
+    }
   }
   confirmPassword.reportValidity();
 }
 
 confirmPassword.addEventListener('keyup', checkPassword);
+document.querySelector(".submit").addEventListener('click', checkPassword);
